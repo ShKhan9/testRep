601d50c41420a45fd039fd6354a37d48aa222901
shehata_gamal <shehata.gamal@poseidon-me.com>
2018-11-15 18:21:21 +0200

    ddssdsdsd

--- tester/qqqqqViewController.swift
+++ tester/qqqqqViewController.swift
@@ -14,6 +14,8 @@ class qqqqqViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // from disk speaking 
     }
     
 
